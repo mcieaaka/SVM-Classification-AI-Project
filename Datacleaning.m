e057bc1129f9c7cb307e9b8e1489a5e63899bf3e
Harshit <harshit0300@gmail.com>
2021-11-13 01:07:26 +0530

    cleaning

--- Datacleaning.m
+++ Datacleaning.m
@@ -1,4 +1,14 @@
 data = readtable("breast-cancer-wisconsin.csv");
 
+len = length(data.class);
+for i = 1:len
+    if(data.class(i)==2)
+        data.class(i)=0;
+    else
+        data.class(i)=1;
+    end
+end 
+    
 
-data
+training_data = data(1:559,:);
+testing_data = data(560:699,:);
